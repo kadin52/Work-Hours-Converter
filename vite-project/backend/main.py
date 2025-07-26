f81448d9e6a2e9dea553ba97599f0deabfa04532
KadinSiphengphone <kadin5553@gmail.com>
2025-07-25 20:32:18 -0700

    product

--- vite-project/backend/main.py
+++ vite-project/backend/main.py
@@ -7,9 +7,12 @@ from vision_service import extract_text
 from file_handler import create_text_download
 
 
-print("before environ")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"E:\Work-Hours-Converter\vite-project\backend\stable-being-455902-h1-ad26de1b685b.json"
-print("after environ")
+credentials_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+if not credentials_json:
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"E:\Work-Hours-Converter\vite-project\backend\stable-being-455902-h1-ad26de1b685b.json"
+
+    
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 CORS(app, supports_credentials=True)
@@ -19,4 +22,4 @@ register_routes(app)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=int(os.environ.get("PORT", 8080)))
