a178e0a37ef270c721896b19de5873c94d2b8316
KadinSiphengphone <kadin5553@gmail.com>
2025-07-26 01:52:56 -0700

    Update http

--- vite-project/src/http-common.tsx
+++ vite-project/src/http-common.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://work-hours-converter.onrender.com",
   headers: {
     "Content-type": "application/json",
   },
