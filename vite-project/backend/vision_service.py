ac0a08cc25f79c0f02546aa566bd4bacfd4bc3c8
KadinSiphengphone <kadin5553@gmail.com>
2025-07-27 00:15:40 -0700

    Update vision_service.py

--- vite-project/backend/vision_service.py
+++ vite-project/backend/vision_service.py
@@ -4,23 +4,23 @@ from google.cloud import vision
 
 
 def extract_text(google_credentials=None):
-    if 'file' not in request.files:
-        return "", FileNotFoundError
-    client = vision.ImageAnnotatorClient(credentials=google_credentials)
+    # if 'file' not in request.files:
+    #     return "", FileNotFoundError
+    # client = vision.ImageAnnotatorClient(credentials=google_credentials)
 
-    file = request.files['file']
-    print(f"Request: {request}")
-    print(f"File: {file}")
-    try:
-        image_read = file.read()
-        image = vision.Image(content=image_read)
-        response = client.document_text_detection(image=image)
-        extracted_text = response.full_text_annotation.text
-        print(f"Extracted text: {extracted_text}")
-        return extracted_text, None
-    except Exception as e:
-        print(f"Error: {e}")
-        return "", str(e)
+    # file = request.files['file']
+    # print(f"Request: {request}")
+    # print(f"File: {file}")
+    # try:
+    #     image_read = file.read()
+    #     image = vision.Image(content=image_read)
+    #     response = client.document_text_detection(image=image)
+    #     extracted_text = response.full_text_annotation.text
+    #     print(f"Extracted text: {extracted_text}")
+    #     return extracted_text, None
+    # except Exception as e:
+    #     print(f"Error: {e}")
+    #     return "", str(e)
 
-    # return "PLACEHOLDER", None
+    return "PLACEHOLDER", None
 
