3f1dd4ce46cc75ec143fe6d0a8d303aba69f542a
KadinSiphengphone <kadin5553@gmail.com>
2025-09-09 14:10:22 -0700

    Delete

--- vite-project/src/pages/ImageToCSV.tsx
+++ vite-project/src/pages/ImageToCSV.tsx
@@ -1,19 +1,8 @@
 import ImageUpload from "../components/ImageUpload";
 import InfoSection from "../components/About";
 import "./SharedImageTo.css";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const ImageToCSV = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const extractedText = localStorage.getItem("extractedText");
-    if (!extractedText) {
-      navigate("/toCsv");
-    }
-  });
-
   return (
     <div className="container">
       <div className="text-center">
